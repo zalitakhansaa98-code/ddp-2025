b88551c63e59dcf8dec1e0f8f6c35bc6475dd5ac
zalitakhansaa98-code <zalitakhansaa98@gmail.com>
2025-11-28 22:18:39 +0700

    Add files via upload

--- bangundatar.py
+++ bangundatar.py
@@ -1,18 +1,26 @@
 import math
 
-def jajargenjang(alas, tinggi):
-    return alas * tinggi
-
 def persegi(sisi):
     return sisi * sisi
 
+print(persegi(9))
+
 def persegi_panjang(panjang, lebar):
     return panjang * lebar
+
+print(persegi_panjang(5,5))
     
 def segitiga(alas, tinggi):
     return 0.5* alas * tinggi
 
+print(segitiga(16,5))
+
 def lingkaran(jari_jari):
     return 3.14 * jari_jari * jari_jari
-    
-    
+
+print(lingkaran(6))
+
+def jajargenjang(alas, tinggi):
+    return alas * tinggi
+
+print(jajargenjang(6,5))
